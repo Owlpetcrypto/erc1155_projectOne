bb0db455865c05f82e76c5776f8d9b50612783ed
Owlpetcrypto <owlpetcrypto@gmail.com>
2022-12-05 00:25:30 +0100

    updated remix contract

--- contracts/ExampleNFT.sol
+++ contracts/ExampleNFT.sol
@@ -5,13 +5,22 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract ExampleNFT is ERC1155, Ownable { //0x28550209Fe15890cB18A8Ac341a8A33b6D18A2B2 contract on testnet
+contract ExampleNFT is ERC1155, Ownable { //0x1CAe00F6B983966F610f7a5f1FE2027E8642C3c5
 
-    uint256 [] public supplies = [10, 10, 10, 10];
-    uint256 [] public minted = [0, 0, 0, 0];
+    
+    uint256 public supplyOne = 10;
+    uint256 public supplyTwo = 10;
+    uint256 public supplyThree = 10;
+    uint256 public supplyFour = 10;
+    uint256 public mintedOne;
+    uint256 public mintedTwo;
+    uint256 public mintedThree;
+    uint256 public mintedFour;
     uint256 public mintPrice;
     address payable public withdrawWallet;
     bool public isPublicMintEnabled;
+    uint256 public maxPerTokenId = 1;
+    uint256 public maxMint = 1;
 
     mapping(address => uint256) public walletMintOne;
     mapping(address => uint256) public walletMintTwo;
@@ -42,9 +51,38 @@ contract ExampleNFT is ERC1155, Ownable { //0x28550209Fe15890cB18A8Ac341a8A33b6D
         isPublicMintEnabled = _isPublicMintEnabled;
     }
 
+    //set supplyOne
+    function setsupplyOne(uint256 _supplyOne) external onlyOwner {
+        supplyOne = _supplyOne;
+    }
+
+    //set supplyTwo
+    function setsupplyTwo(uint256 _supplyTwo) external onlyOwner {
+        supplyTwo = _supplyTwo;
+    }
+
+    //set supplyThree
+    function setsupplyThree(uint256 _supplyThree) external onlyOwner {
+        supplyThree = _supplyThree;
+    }
+
+    //set supplyFour
+    function setsupplyFour(uint256 _supplyFour) external onlyOwner {
+        supplyFour = _supplyFour;
+    }
     //set mintPrice
     function setMintPrice(uint256 _mintPrice) external onlyOwner {
         mintPrice = _mintPrice;
+    }
+
+    //set maxPerTokenId
+    function setMaxPerTokenId(uint256 _maxPerTokenId) external onlyOwner {
+        maxPerTokenId = _maxPerTokenId;
+    }
+
+    //set maxMint
+    function setMaxMint(uint256 _maxMint) external onlyOwner {
+        maxMint = _maxMint;
     }
 
     //set withdraw wallet
@@ -58,84 +96,71 @@ contract ExampleNFT is ERC1155, Ownable { //0x28550209Fe15890cB18A8Ac341a8A33b6D
         require(success, 'withdraw failed');
     }
 
-    //mint
-    function mintOne() external payable {
-        uint256 id = 1;
-        uint256 amount = 1;
+    //clean up code to avoid duplications
+    function mint(uint256 _id, uint256 _amount) internal {
         require(isPublicMintEnabled, "Minting is not live");
-        require(id <= supplies.length, "Token does not exist");
-        require(id > 0, "Token does not exist");
-       
-        uint256 index = id - 1;
-        require(minted[index] + amount <= supplies[index], "Not enough supplies");
-        require(msg.value == mintPrice * amount, "Insufficient funds");
-        require(walletMintOne[msg.sender] < 1,"Already minted");
-
-        _mint(msg.sender, id, amount, '');
-       
-        walletMintOne[msg.sender] ++;
-        minted[index] += amount;
+        require(msg.value == mintPrice * _amount, "Insufficient funds");
+        require(_amount <= maxPerTokenId, "Exceed max mint");
+        _mint(msg.sender, _id, _amount, '');
     }
 
-        function mintTwo() external payable {
+    //mintOne
+    function mintOne(uint256 _amount) external payable {
 
-        uint256 id = 2;
-        uint256 amount = 1;
+        uint256 _id = 1;
+
+        require(mintedOne + _amount <= supplyOne, "Not enough supplies");
+        require(walletMintOne[msg.sender] < maxMint,"Already minted");
+
+        mint(_id, _amount);
         
-        require(isPublicMintEnabled, "Minting is not live");
-        require(id <= supplies.length, "Token does not exist");
-        require(id > 0, "Token does not exist");
-       
-        uint256 index = id - 1;
-        require(minted[index] + amount <= supplies[index], "Not enough supplies");
-        require(msg.value == mintPrice * amount, "Insufficient funds");
-        require(walletMintTwo[msg.sender] < 1,"Already minted");
-
-        _mint(msg.sender, id, amount, '');
-       
-        walletMintTwo[msg.sender] ++;
-        minted[index] += amount;
+        walletMintOne[msg.sender] ++;   
+        mintedOne += _amount;
     }
 
-        function mintThree() external payable {
-       
-        uint256 id = 3;
-        uint256 amount = 1;
-       
-        require(isPublicMintEnabled, "Minting is not live");
-        require(id <= supplies.length, "Token does not exist");
-        require(id > 0, "Token does not exist");
-       
-        uint256 index = id - 1;
-        require(minted[index] + amount <= supplies[index], "Not enough supplies");
-        require(msg.value == mintPrice * amount, "Insufficient funds");
-        require(walletMintThree[msg.sender] < 1,"Already minted");
+    //mintTwo
+    function mintTwo(uint256 _amount) external payable {
 
-        _mint(msg.sender, id, amount, '');
-       
-        walletMintThree[msg.sender] ++;
-        minted[index] += amount;
-    }
+        uint256 _id = 2;
 
-        function mintFour() external payable {
+        require(mintedTwo + _amount <= supplyTwo, "Not enough supplies");
+        require(walletMintTwo[msg.sender] < maxMint,"Already minted");
+
+        mint(_id, _amount);
         
-        uint256 id = 4;
-        uint256 amount = 1;
-        
-        require(isPublicMintEnabled, "Minting is not live");
-        require(id <= supplies.length, "Token does not exist");
-        require(id > 0, "Token does not exist");
-       
-        uint256 index = id - 1;
-        require(minted[index] + amount <= supplies[index], "Not enough supplies");
-        require(msg.value == mintPrice * amount, "Insufficient funds");
-        require(walletMintFour[msg.sender] < 1,"Already minted");
-
-        _mint(msg.sender, id, amount, '');
-        walletMintFour[msg.sender] ++;
-    
-        minted[index] += amount;
+        walletMintTwo[msg.sender] ++;   
+        mintedTwo += _amount;
     }
+
+    //mintThree
+    function mintThree(uint256 _amount) external payable {
+
+        uint256 _id = 3;
+
+        require(mintedThree + _amount <= supplyThree, "Not enough supplies");
+        require(walletMintThree[msg.sender] < maxMint,"Already minted");
+
+        mint(_id, _amount);
+        
+        walletMintThree[msg.sender] ++;   
+        mintedThree += _amount;
+    }
+
+    //mintFour
+    function mintFour(uint256 _amount) external payable {
+
+        uint256 _id = 4;
+
+        require(mintedFour + _amount <= supplyFour, "Not enough supplies");
+        require(walletMintFour[msg.sender] < maxMint,"Already minted");
+
+        mint(_id, _amount);
+        
+        walletMintFour[msg.sender] ++;   
+        mintedFour += _amount;
+    }
+
+
 }
 
 //thoughts if there are four mints - after minting one they should be able to mint the other 3
--- src/MainMint.js
+++ src/MainMint.js
@@ -3,13 +3,13 @@ import {ethers, BigNumber} from 'ethers';
 import { Box,  Flex, Button, Text, Image } from '@chakra-ui/react';
 import ExampleNFT from './ExampleNFT.json';
 
-const ExampleNFTAddress = "0x6Fb611700075151a676572A3e31758F84aEa12A7";
+const ExampleNFTAddress = "0x1CAe00F6B983966F610f7a5f1FE2027E8642C3c5";
 
 const MainMint = ({ accounts, setAccounts }) => {
     const[mintAmount, setMintAmount] = useState(1);
     const isConnected = Boolean(accounts[0]);
 
-    async function handleMint() {
+    async function handleMintOne() {
         if (window.ethereum) {
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             const signer = provider.getSigner();
@@ -18,10 +18,69 @@ const MainMint = ({ accounts, setAccounts }) => {
                 ExampleNFT.abi,
                 signer
             );
-            //HOW TO SOLVE?????????????????????????????????????????????????
             try {
-                const response = await contract.mintOne( {
-                    value: ethers.utils.parseEther((0.001).toString()),
+                const response = await contract.mintOne(BigNumber.from(mintAmount), {
+                    value: ethers.utils.parseEther((0.001 * mintAmount).toString()),
+                });
+                console.log('response: ', response);
+            } catch (err) {
+                console.log("error: ", err)
+            }
+        }
+    }
+
+    async function handleMintTwo() {
+        if (window.ethereum) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(
+                ExampleNFTAddress,
+                ExampleNFT.abi,
+                signer
+            );
+            try {
+                const response = await contract.mintTwo(BigNumber.from(mintAmount), {
+                    value: ethers.utils.parseEther((0.001 * mintAmount).toString()),
+                });
+                console.log('response: ', response);
+            } catch (err) {
+                console.log("error: ", err)
+            }
+        }
+    }
+
+    async function handleMintThree() {
+        if (window.ethereum) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(
+                ExampleNFTAddress,
+                ExampleNFT.abi,
+                signer
+            );
+            try {
+                const response = await contract.mintThree(BigNumber.from(mintAmount), {
+                    value: ethers.utils.parseEther((0.001 * mintAmount).toString()),
+                });
+                console.log('response: ', response);
+            } catch (err) {
+                console.log("error: ", err)
+            }
+        }
+    }
+
+    async function handleMintFour() {
+        if (window.ethereum) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(
+                ExampleNFTAddress,
+                ExampleNFT.abi,
+                signer
+            );
+            try {
+                const response = await contract.mintFour(BigNumber.from(mintAmount), {
+                    value: ethers.utils.parseEther((0.001 * mintAmount).toString()),
                 });
                 console.log('response: ', response);
             } catch (err) {
@@ -34,20 +93,27 @@ const MainMint = ({ accounts, setAccounts }) => {
         <Flex justify='center' align='center' height="50vh" paddingBottom='150px' >
             <Box width='1000px'>
                 <div>
-                    <Image src="https://gateway.pinata.cloud/ipfs/QmV61wWswuVsome2PQbfNPHZ912aBi7vUjn7wTg8yyaMQC/1.png" width="35%" marginTop="500px" borderRadius="10%">
+                    <Image src="https://gateway.pinata.cloud/ipfs/QmV61wWswuVsome2PQbfNPHZ912aBi7vUjn7wTg8yyaMQC/1.png" width="80%" marginTop="500px" borderRadius="10%">
 
                     </Image>
                     <Text fontSize="20px" textShadow="0 5px #00000" color="black">
                         ExampleNFT #1
                     </Text>
                   
+                    {/* <Text 
+                        fontSize="30px" 
+                        fontFamily="inherit" 
+                        textShadow="0 2px 2px #00000"
+                        color="black">
+                        Testing 1, 2, 3, 4
+                    </Text> */}
                 </div>
 
                 {isConnected ? (
                     <div>
                         <div>
                             <Text
-                                //readyOnly 
+                                readyOnly 
                                 fontFamily="inherit" 
                                 // width="85px" 
                                 // height="40px" 
@@ -71,7 +137,196 @@ const MainMint = ({ accounts, setAccounts }) => {
                                 padding="15px"
                                 // marginTop="10px"
                                 fontSize="20px" 
-                                onClick={handleMint}>
+                                onClick={handleMintOne}>
+                                Mint Now
+                            </Button>
+                    </div>
+
+                    
+                ) : (
+                    <Text 
+                        marginTop="50px"
+                        fontSize="30px"
+                        fontFamily="inherit"
+                        textShadow="0 3px #000000"
+                        color='black'>
+                        {/* You must be connected to Mint. */}
+                    </Text>
+                )}
+            </Box>
+            <Box width='1000px'>
+                <div>
+                    <Image src="https://gateway.pinata.cloud/ipfs/QmV61wWswuVsome2PQbfNPHZ912aBi7vUjn7wTg8yyaMQC/2.png" width="80%" marginTop="500px" borderRadius="10%">
+
+                    </Image>
+                    <Text fontSize="20px" textShadow="0 5px #00000" color="black">
+                    ExampleNFT #2
+                    </Text>
+                    {/* <Text 
+                        fontSize="30px" 
+                        fontFamily="inherit" 
+                        textShadow="0 2px 2px #00000"
+                        color="black">
+                        Testing 1, 2, 3, 4
+                    </Text> */}
+                </div>
+
+                {isConnected ? (
+                    <div>
+                        <div>
+                            <Text
+                                readyOnly 
+                                fontFamily="inherit" 
+                                // width="85px" 
+                                // height="40px" 
+                                textAlign="center" 
+                                // paddingLeft="19px" 
+                                // marginTop="10px" 
+                                fontSize="30px" 
+                                type="number">
+                                
+                                {mintAmount} / 10
+
+                            </Text> 
+                        </div>
+                            <Button 
+                                backgroundColor="#0E0D0B"
+                                borderRadius="5px"
+                                boxShadow="0px 2px 2px 1px #0F0F0F"
+                                color="inherit"
+                                cursor="pointer"
+                                fontFamily="inherit"
+                                padding="15px"
+                                // marginTop="10px"
+                                fontSize="20px" 
+                                onClick={handleMintTwo}>
+                                Mint Now
+                            </Button>
+                    </div>
+
+                    
+                ) : (
+                    <Text 
+                        marginTop="50px"
+                        fontSize="30px"
+                        fontFamily="inherit"
+                        textShadow="0 3px #000000"
+                        color='black'>
+                        {/* You must be connected to Mint. */}
+                    </Text>
+                )}
+            </Box>
+
+            <Box width='1000px'>
+                <div>
+                    <Image src="https://gateway.pinata.cloud/ipfs/QmV61wWswuVsome2PQbfNPHZ912aBi7vUjn7wTg8yyaMQC/3.png" width="80%" marginTop="500px" borderRadius="10%">
+
+                    </Image>
+                    <Text fontSize="20px" textShadow="0 5px #00000" color="black">
+                        ExampleNFT #3
+                    </Text>
+                    {/* <Text 
+                        fontSize="30px" 
+                        fontFamily="inherit" 
+                        textShadow="0 2px 2px #00000"
+                        color="black">
+                        Testing 1, 2, 3, 4
+                    </Text> */}
+                </div>
+
+                {isConnected ? (
+                    <div>
+                        <div>
+                            <Text
+                                readyOnly 
+                                fontFamily="inherit" 
+                                // width="85px" 
+                                // height="40px" 
+                                textAlign="center" 
+                                // paddingLeft="19px" 
+                                // marginTop="10px" 
+                                fontSize="30px" 
+                                type="number">
+                                
+                                {mintAmount} / 10
+
+                            </Text> 
+                        </div>
+                            <Button 
+                                backgroundColor="#0E0D0B"
+                                borderRadius="5px"
+                                boxShadow="0px 2px 2px 1px #0F0F0F"
+                                color="inherit"
+                                cursor="pointer"
+                                fontFamily="inherit"
+                                padding="15px"
+                                // marginTop="10px"
+                                fontSize="20px" 
+                                onClick={handleMintThree}>
+                                Mint Now
+                            </Button>
+                    </div>
+
+                    
+                ) : (
+                    <Text 
+                        marginTop="50px"
+                        fontSize="30px"
+                        fontFamily="inherit"
+                        textShadow="0 3px #000000"
+                        color='black'>
+                        {/* You must be connected to Mint. */}
+                    </Text>
+                )}
+            </Box>
+
+            <Box width='1000px'>
+                <div>
+                    <Image src="https://gateway.pinata.cloud/ipfs/QmV61wWswuVsome2PQbfNPHZ912aBi7vUjn7wTg8yyaMQC/4.png" width="80%" marginTop="500px" borderRadius="10%">
+
+                    </Image>
+                    <Text fontSize="20px" textShadow="0 5px #00000" color="black">
+                        ExampleNFT #4
+                    </Text>
+                    
+                    {/* <Text 
+                        fontSize="30px" 
+                        fontFamily="inherit" 
+                        textShadow="0 2px 2px #00000"
+                        color="black">
+                        Testing 1, 2, 3, 4
+                    </Text> */}
+                </div>
+
+                {isConnected ? (
+                    <div>
+                        <div>
+                            <Text
+                                readyOnly 
+                                fontFamily="inherit" 
+                                // width="85px" 
+                                // height="40px" 
+                                textAlign="center" 
+                                // paddingLeft="19px" 
+                                // marginTop="10px" 
+                                fontSize="30px" 
+                                type="number">
+                                
+                                {mintAmount} / 10
+
+                            </Text> 
+                        </div>
+                            <Button 
+                                backgroundColor="#0E0D0B"
+                                borderRadius="5px"
+                                boxShadow="0px 2px 2px 1px #0F0F0F"
+                                color="inherit"
+                                cursor="pointer"
+                                fontFamily="inherit"
+                                padding="15px"
+                                // marginTop="10px"
+                                fontSize="20px" 
+                                onClick={handleMintFour}>
                                 Mint Now
                             </Button>
                     </div>
@@ -90,6 +345,8 @@ const MainMint = ({ accounts, setAccounts }) => {
             </Box>
         </Flex>
     );
+
+
 };
 
 export default MainMint;
